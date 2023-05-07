67320a898ce33b2819d2bb9506e19be777ba5d72
famutimine <62528290+famutimine@users.noreply.github.com>
2023-05-06 23:56:50 -0500

    Update pdcscore.py

--- pdcscore/pdcscore.py
+++ pdcscore/pdcscore.py
@@ -1,73 +1,68 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 from multiprocessing import Pool, cpu_count
 
-class pdccalc:
+class pdcCalc:
     def __init__(self, dataframe, patient_id_col, drugname_col, filldate_col,
-                          supply_days_col, mbr_elig_start_dt_col,
-                          mbr_elig_end_dt_col):
+                 supply_days_col, msr_start_dt_col, msr_end_dt_col):
         self.dataframe = dataframe
         self.patient_id_col = patient_id_col
         self.drugname_col = drugname_col
         self.filldate_col = filldate_col
         self.supply_days_col = supply_days_col
-        self.mbr_elig_start_dt_col = mbr_elig_start_dt_col
-        self.mbr_elig_end_dt_col = mbr_elig_end_dt_col
-        self.patient_ids = set(dataframe[patient_id_col])
-        self.drugs = set(dataframe[drugname_col])
-        self.pdc_scores_df = pd.DataFrame(columns=[patient_id_col, drugname_col, 'DAYSCOVERED', 'TOTALDAYS', 'PDC_SCORE'])
-        
-    def calculate_pdc_for_patient_drug(self, patient_id, drug):
-        subset = self.dataframe[(self.dataframe[self.patient_id_col] == patient_id) & (self.dataframe[self.drugname_col] == drug)]
-        if subset.empty:
-            return None
-        
-        # determine if the first fill date precedes mbr_elig_start_dt_col by <=6 months
-        first_fill_date = subset[self.filldate_col].min()
-        mbr_elig_start_dt = subset[self.mbr_elig_start_dt_col].iloc[0]
-        if (first_fill_date - mbr_elig_start_dt) <= pd.Timedelta(days=6*30):
-            start_date = mbr_elig_start_dt
-        else:
-            start_date = max(first_fill_date, mbr_elig_start_dt)
-        
-        dates = []
-        for i, row in subset.iterrows():
-            end_date = min(row[self.filldate_col] + pd.Timedelta(days=row[self.supply_days_col]), row[self.mbr_elig_end_dt_col])
-            if end_date >= start_date:
-                dates.append((start_date, end_date))
-        dates.sort()
-        merged_dates = [dates[0]]
-        for date_range in dates[1:]:
-            if date_range[0] <= merged_dates[-1][1]:
-                merged_dates[-1] = (merged_dates[-1][0], max(date_range[1], merged_dates[-1][1]))
-            else:
-                merged_dates.append(date_range)
-        total_covered = sum([(date_range[1] - date_range[0]).days + 1 for date_range in merged_dates])
-        total_days = (subset[self.mbr_elig_end_dt_col].iloc[0] - subset[self.mbr_elig_start_dt_col].iloc[0]).days + 1
-        # set PDC_SCORE to zero if TOTALDAYS or DAYSCOVERED is zero
-        if total_days == 0 or total_covered == 0:
-            proportion = 0
-        else:
-            proportion = total_covered / total_days
-        return {self.patient_id_col: patient_id,
-                self.drugname_col: drug,
-                'DAYSCOVERED': total_covered,
-                'TOTALDAYS': total_days,
-                'PDC_SCORE': proportion}
-        
+        self.msr_start_dt_col = msr_start_dt_col
+        self.msr_end_dt_col = msr_end_dt_col
+
     def calculate_pdc(self, n_workers=cpu_count()):
         pool = Pool(processes=n_workers)
-        results = []
-        for patient_id in self.patient_ids:
-            for drug in self.drugs:
-                results.append(pool.apply_async(self.calculate_pdc_for_patient_drug, (patient_id, drug)))
-        pool.close()
-        pool.join()
+        df = self.dataframe.copy()
+
+        # Calculate the date difference
+        df['date_diff'] = df[self.msr_start_dt_col] - df[self.filldate_col] + pd.Timedelta(days=1)
+
+        # Filter rows where date difference is less than or equal to 180 days
+        df = df[df['date_diff'] < pd.Timedelta(days=180)]
+
+        # Remove the date_diff column
+        df = df.drop('date_diff', axis=1)
+
+        # Get the fill_enddate
+        df['fill_enddate'] = df[self.filldate_col] + pd.to_timedelta(df[self.supply_days_col], unit='D')
+
+        # Group the dataframe and select the first value of each column within each group
+        new_df = df.groupby([self.patient_id_col, self.drugname_col]).first().reset_index()
+
+        new_df['effective_msr_start_dt'] = new_df.apply(lambda row: row[self.filldate_col]
+                                                        if row[self.filldate_col] > row[self.msr_start_dt_col]
+                                                        else row[self.msr_start_dt_col], axis=1)
+
+        # Calculate the totaldays: denominator for each patient-drugname pair
+        new_df['totaldays'] = (new_df[self.msr_end_dt_col] + pd.Timedelta(days=1) - new_df[
+            'effective_msr_start_dt']).dt.days
+
+        df = df.merge(new_df[[self.patient_id_col, self.drugname_col, 'effective_msr_start_dt', 'totaldays']],
+                      on=[self.patient_id_col, self.drugname_col], how='left')
+
+        # Create a date array for each row that represents all valid rows based on logic below:
+        # if the filldate_col is less than effective_msr_start_dt (effective start date of the measurement period) then use effective_msr_start_dt
+        # else use filldate_col as the start of the range, the end date of the range is the fill_enddate less of msr_end_dt_col
+
+        def generate_date_array(row):
+            date_range = pd.date_range(start=row['effective_msr_start_dt']
+                                       if row[self.filldate_col] < row['effective_msr_start_dt']
+                                       else row[self.filldate_col],
+                                       end=row['fill_enddate'])
+            return [str(date.date()) for date in date_range if date <= row[self.msr_end_dt_col]]
+
+        df['date_array'] = [generate_date_array(row) for _, row in df.iterrows()]
         
-        for result in results:
-            data = result.get()
-            if data is not None:
-                self.pdc_scores_df = pd.concat([self.pdc_scores_df, pd.DataFrame.from_records([data])])
-                
-        self.pdc_scores_dataframe = self.pdc_scores_df.sort_values(by=[self.patient_id_col, self.drugname_col]).reset_index(drop=True)
-        return self.pdc_scores_dataframe
+        # Group by 'patient_id_col', 'drugname_col', 'totaldays' and aggregate by a concatenated list of unique dates (to avoid overlapping dates)
+        # the array length represents the dayscovered
+        pdc = df.groupby([self.patient_id_col, self.drugname_col, 'totaldays'])['date_array'].apply(
+            lambda x: len(np.unique(np.concatenate(x.tolist())))).reset_index(name='dayscovered')
+
+        # Calculate pdc column as dayscovered divided by totaldays
+        pdc['pdc_score'] = pdc['dayscovered'] / pdc['totaldays']
+
+        return pdc
+
